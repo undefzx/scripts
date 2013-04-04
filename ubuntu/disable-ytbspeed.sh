5ccafc6fa47bf3930a9ea023712470b0ab61f63a
Dmitriy Boyarshinov <dmitriy.boyarshinov@gmail.com>
2013-04-04 21:14:00 +0400

    Блокирование кэширующих серверов youtube для большей скорости

--- ubuntu/disable-ytbspeed.sh
+++ ubuntu/disable-ytbspeed.sh
@@ -1,2 +1,4 @@
-sudo iptables -I FORWARD -s 173.194.55.0/24 -j DROP
+sudo iptables -I INPUT -s 206.111.0.0/16 -j DROP
 sudo iptables -I FORWARD -s 206.111.0.0/16 -j DROP
+sudo iptables -I INPUT -s 173.194.55.0/24 -j DROP
+sudo iptables -I FORWARD -s 173.194.55.0/24 -j DROP
